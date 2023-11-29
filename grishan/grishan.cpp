ba7a11ae48c0772c5eedb542ef133f676f917983
AlwaysNasty <angrykd00@gmail.com>
2023-11-30 01:54:30 +0300

    added

--- grishan/grishan.cpp
+++ grishan/grishan.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int factorial(int a)
+{
+    if (a < 1)
+        return 1;
+
+    return a * factorial(a - 1);
+}
+
+int phi(int n)
+{
+    int result = n;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
 int main()
 {
-    int main;
+    cout << phi(5) << endl;
+    cout << factorial(6);
     return 0;
 }
