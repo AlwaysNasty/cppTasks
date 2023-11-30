fc300e55d01dbde64d27849759d524566dd076cb
AlwaysNasty <angrykd00@gmail.com>
2023-11-30 14:12:07 +0300

    added grisha task

--- grishan/grishan.cpp
+++ grishan/grishan.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    int problems = 12;
+    int problems = 12, time_m, f;
+    cin >> f;
+
+    int rest = problems - f; // оставшиеся задачи
+
+    time_m = 240; // оставшееся время
+
+    int result = time_m - 45 * rest;
+
+    if (result >= 0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 
     return 0;
 }
